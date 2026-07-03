5b27da628cc7bc1939f4137c8d61e937c90e7c9a
ayesha9753 <bc210402706aim@vu.edu.pk>
2026-07-04 01:08:27 +0500

    feat: add product model and create product API

--- app/backend/app/routers/products.py
+++ app/backend/app/routers/products.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
-from app.services.product_service import get_all_products
+from app.models.product import Product
+from app.services.product_service import (
+    get_all_products,
+    create_product
+)
 
 router = APIRouter()
+
 
 @router.get("/products")
 def get_products():
     return get_all_products()
 
+
+@router.post("/products")
+def add_product(product: Product):
+    return create_product(product)
--- app/backend/app/services/product_service.py
+++ app/backend/app/services/product_service.py
@@ -1,13 +1,27 @@
+products = [
+    {
+        "id": 1,
+        "name": "MacBook Pro M4",
+        "price": 450000
+    },
+    {
+        "id": 2,
+        "name": "Logitech MX Master 3S",
+        "price": 32000
+    }
+]
+
+
 def get_all_products():
-    return [
-        {
-            "id": 1,
-            "name": "MacBook Pro M4",
-            "price": 450000
-        },
-        {
-            "id": 2,
-            "name": "Logitech MX Master 3S",
-            "price": 32000
-        }
-    ]
+    return products
+
+
+def create_product(product):
+    new_product = {
+        "id": len(products) + 1,
+        "name": product.name,
+        "price": product.price
+    }
+
+    products.append(new_product)
+    return new_product
